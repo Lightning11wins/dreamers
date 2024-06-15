50baf23b751bc7e62a259e11434c299144abc8bd
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-15 17:00:16 -0500

    Add logging util

---
+++ utils.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const logDir = 'logs';
+
+class Logger {
+	constructor() {
+		const now = Date.now().toString();
+		this.filename = `log-${now}.txt`;
+		this.filepath = path.join(logDir, this.filename);
+
+		fs.writeFileSync(this.filepath, `Start of ${this.filename}:\n\n`);
+	}
+
+	log(msg) {
+		const now = Date.now().toString();
+		const data = `[${now}]: ${msg}\n`;
+		fs.appendFile(this.filepath, data, (err) => {
+			if (err) {
+				console.error(`Failed to write to log file at ${this.filepath}:`, err);
+				console.log('Data:', data);
+			}
+		});
+	}
+}
+
+module.exports = {
+	logDir,
+	Logger,
+}
