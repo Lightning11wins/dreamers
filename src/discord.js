f715d26c0891aa6e0f287524302e4b8da6b7926d
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-15 17:02:23 -0500

    Refactor

--- src/discord.js
+++ src/discord.js
@@ -1,5 +1,5 @@
 
-const { headers } = require('private');
+const { headers } = require('src/private');
 
 function send(msg) {
 	fetch("https://discord.com/api/v9/channels/1196575384804802661/messages", {
