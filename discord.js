33369046577aeb8f43de3cc590e78b4af98aaf32
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-15 17:00:35 -0500

    Add logic to send Discord messages.

---
+++ discord.js
@@ -0,0 +1,12 @@
+
+const { headers } = require('private');
+
+function send(msg) {
+	fetch("https://discord.com/api/v9/channels/1196575384804802661/messages", {
+		headers, body: `{"content":"${msg}"}`, method: "POST",
+	}).then();
+}
+
+module.exports = {
+	send,
+}
