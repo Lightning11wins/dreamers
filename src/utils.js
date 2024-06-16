3e650435f315f682cfce3ab1f39267d759b5bc7a
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-16 15:09:37 -0500

    Fix time formatting.

--- src/utils.js
+++ src/utils.js
@@ -20,7 +20,7 @@ function now(format) {
 		const hours = String(now.getHours()).padStart(2, '0');
 		const minutes = String(now.getMinutes()).padStart(2, '0');
 		const seconds = String(now.getSeconds()).padStart(2, '0');
-		str += `${showDate ? '_' : ''}${hours}:${minutes}:${seconds}`;
+		str += `${showDate ? '_' : ''}${hours}-${minutes}-${seconds}`;
 	}
 	return str;
 }
