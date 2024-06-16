a70985e7f09d0a09207812e1cb34908423102087
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-15 20:34:02 -0500

    Allow the bot to repeat users' messages.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 
 const { Logger } = require('./utils');
-const { Discord } = require('./discord');
-const { ollama } = require('./ai');
+const { Discord, channel } = require('./discord');
+// const { ollama } = require('./ai');
 
 const logger = new Logger();
 logger.log('Program started');
 
 const discord = new Discord();
 discord.system = 'Program started';
+
+function annoy() {
+	if (discord.isSending) return;
+	discord.get(channel.general).then((data) => {
+		if (data[0].handle !== 'evs17.') {
+			discord.general = data[0].text;
+		}
+	});
+}
+
+setInterval(annoy, 5000);
 
 // ollama('What pet should I get?').then(((answer) => console.log('program:', answer)));
